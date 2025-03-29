816fda8e2f4b1aac04aca71c020ea5c63560e09d
Kenny <kennethmadondo01gmail.com>
2025-03-29 08:46:38 +0200

    post route errors

--- src/routes/postsRoute.ts
+++ src/routes/postsRoute.ts
@@ -1,14 +1,14 @@
 import express from 'express';
-import { savePost,fetchPosts,editpost,deletePost,allPosts,postDetails, likePost} from '../controllers/postsController';
+// import { savePost,fetchPosts,editpost,deletePost,allPosts,postDetails, likePost} from '../controllers/postsController';
 // import {uploadPostImages} from '../services/awsconfig'
 
 const router = express.Router();
 // router.post('/savepost',uploadPostImages.array('images',10),savePost)
 // router.patch('/editpost',uploadPostImages.array('images',10),editpost)
-router.get('/post',fetchPosts)
-router.get('/allposts',allPosts)
-router.get('/postdetails/:post_id',postDetails)
-router.delete('/deletepost/:post_id',deletePost)
-router.post('/like/:post_id',likePost)
+// router.get('/post',fetchPosts)
+// router.get('/allposts',allPosts)
+// router.get('/postdetails/:post_id',postDetails)
+// router.delete('/deletepost/:post_id',deletePost)
+// router.post('/like/:post_id',likePost)
 
 export default router;
